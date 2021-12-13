d1a4fa2087d87b7230da438e9ab06274e9141585
Vladyslav-Eduard Svystun <svvladyslaved@gmail.com>
2021-12-14 00:00:43 +0200

    CE24-25

---
+++ CE25.js
@@ -0,0 +1,12 @@
+const binarySearch = (arr, elem) => {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        elem < arr[middle] ? end = middle - 1 : start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 2));
